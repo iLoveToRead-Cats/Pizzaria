c597c5c5800e17cdd5d6ec055bb536fdc1dbf1fc
iLoveToRead-Cats <75917493+iLoveToRead-Cats@users.noreply.github.com>
2021-02-21 19:33:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var menu_list_array = 
+["Cheese Pizza", "Margherita Pizza", "Neapolitan Pizza", "Sicilian Pizza", "Vegetable Pizza"]
+
+function getmenu() {
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++) {
+        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML=htmldata;
+}
+
+function additem() {
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0; i<menu_list_array.length;i++) {
+        htmldata=htmldata+'<div class="card">' + '<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
